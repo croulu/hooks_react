ebb5f7ab30fc2643c981c58019114e6d623b079c
Sylvie PONTHUS-VIOLLAND <sponthus@gmail.com>
2022-02-14 11:27:14 +0100

    state destructuring

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import './App.css';
 import React, {Component} from "react";
 
 class App extends Component {
+    state = {
+        name: 'Lulu',
+        age: 36
+    }
+
     render() {
-        const name = "Luc"
+        const {name, age} = this.state
         return (
-            <h1>Hello {name}</h1>
+            <h1>Hello {name} - {age}</h1>
         )
     }
 }
